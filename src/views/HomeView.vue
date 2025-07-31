62c8055f3715968fbf8be4314d322f9a0d9e8bf3
Faeq <faeqfaisal@hotmail.co.uk>
2025-07-31 16:47:14 +0100

    fix(theme): :dizzy: animating between sites on year change to avoid flashing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,16 @@ import { ref } from 'vue';
 
 // TODO: detect iframe theme both ways, depending on year loaded
 
+const show = ref(true);
 const frame = ref()
 const baseURL = location.origin
 const frameSrc = ref(baseURL + '/PersonalSite2023')
 const yearChange = async (year: string) => {
+  frame.value.style.opacity = 0
+  await sleep(300)
   frameSrc.value = baseURL + '/PersonalSite' + year
+  await sleep(300)
+  frame.value.style.opacity = 1
 }
 
 //applying the right theme to the page in the iframe and showing the frame
@@ -32,15 +37,28 @@ function applyThemeToFrame() {
   }
 }
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 </script>
 
 <template>
   <div id="CurrentSite" class="h-screen w-screen p-0 m-0 absolute left-0 top-0">
     <Transition>
       <iframe class="w-screen h-screen" style="pointer-events: all;" ref="frame"
-        :src="frameSrc" @load="applyThemeToFrame" />
+        :src="frameSrc" @load="applyThemeToFrame" v-if="show" />
     </Transition>
   </div>
 
   <Archive @change="yearChange" />
 </template>
+
+<style>
+iframe {
+  opacity: 1;
+  transition-duration: 300ms;
+  transition-property: opacity;
+  transition-timing-function: ease-in-out;
+}
+</style>
